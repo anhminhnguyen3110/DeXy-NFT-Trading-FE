15d238d70555c60a47bdde602089408e3c3ee0d6
Legacy107 <quocmai1107@gmail.com>
2023-08-18 00:11:26 +1000

    feat: [IN-32] add header with search bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,73 @@
+import { InputBase, alpha, styled } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search'
+
+const Search = styled('div')(({ theme }) => ({
+  maxHeight: '2.5rem',
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.1),
+  },
+  transition: theme.transitions.create('width'),
+  width: '2.5rem',
+  [theme.breakpoints.down('sm')]: {
+    '&:has(.MuiInputBase-input:focus)': {
+      width: '100%',
+      border: `1px solid ${theme.palette.grey[400]}`,
+    },
+  },
+  [theme.breakpoints.up('sm')]: {
+    border: `1px solid ${theme.palette.grey[400]}`,
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}))
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  [theme.breakpoints.down('sm')]: {
+    padding: '0.5rem',
+  },
+}))
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    backgroundColor: 'transparent',
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.down('sm')]: {
+      opacity: 0,
+      width: 0,
+      '&:focus': {
+        opacity: 1,
+        width: '100%',
+      },
+    },
+  },
+  width: '100%',
+}))
+
+export default function SearchBar({ value, handleChange }) {
+  return (
+    <Search>
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+      <StyledInputBase
+        placeholder="Search…"
+        inputProps={{ 'aria-label': 'search' }}
+        value={value}
+        onChange={handleChange}
+      />
+    </Search>
+  )
+}
--- src/layouts/AppBar.js
+++ src/layouts/AppBar.js
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { useRouter } from 'next/router'
+import useResponsive from '@/hooks/useResponsive'
+import Link from 'next/link'
 import {
   Container,
   AppBar,
@@ -11,19 +13,91 @@ import {
   Menu,
   MenuItem,
   styled,
+  Stack,
 } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
+import AccountBalanceWalletRoundedIcon from '@mui/icons-material/AccountBalanceWalletRounded'
 import EtheriumIcon from '@/components/EtheriumIcon'
+import SearchBar from '@/components/SearchBar'
 
 const AppBarStyled = styled(AppBar)(({ theme }) => ({
   backgroundColor: theme.palette.background.neutral,
 }))
 
-const pages = [['Home', '/']]
+const Logo = styled(Typography)(({ theme }) => ({
+  fontFamily: 'Teko, sans-serif',
+  color: theme.palette.primary.main,
+  fontSize: '2.75rem',
+  fontWeight: 600,
+}))
+
+const ToolbarStyled = styled(Toolbar)(({ theme }) => ({
+  minHeight: '4.125rem',
+  gap: '2.8rem',
+  [theme.breakpoints.down('md')]: {
+    gap: '1.5rem',
+  },
+  [theme.breakpoints.down('sm')]: {
+    '&:has(.MuiInputBase-input:focus)': {
+      '& #logo-link, & #nav-menu-button': {
+        display: 'none',
+      },
+    },
+  },
+}))
+
+const NavMenuContainer = styled(Box)(({ theme }) => ({
+  flexGrow: 1,
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+  gap: '0.75rem',
+  [theme.breakpoints.down('sm')]: {
+    gap: '0.1rem',
+  },
+}))
+
+const NavButton = styled(Button)(({ theme }) => ({
+  position: 'relative',
+  top: '-0.25rem',
+  display: 'block',
+  fontSize: '1.25rem',
+  fontWeight: 400,
+  color: theme.palette.text.primary,
+  '&:hover': {
+    backgroundColor: 'unset',
+  },
+  '&::before': {
+    content: '""',
+    position: 'absolute',
+    bottom: 0,
+    width: 0,
+    height: '0.25rem',
+    backgroundColor: theme.palette.primary.main,
+    borderRadius: '100px',
+    transition: 'width 0.15s ease-in-out',
+  },
+  '&:hover::before': {
+    width: 'max(20%, 1.75rem)',
+  },
+  '&.nav-button-active::before': {
+    width: 'calc(100% - 16px)',
+  },
+}))
+
+const pages = [
+  ['Marketplace', '/'],
+  ['Create', '/create'],
+]
+const accountMenu = [
+  ['Create wallet', 'https://metamask.io/'],
+  ['Account', '/account'],
+]
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = useState(null)
   const router = useRouter()
+  const isDesktop = useResponsive('up', 'md')
+  const [searchValue, setSearchValue] = useState('')
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget)
@@ -38,21 +112,29 @@ function ResponsiveAppBar() {
     router.push(`${location.toLowerCase()}`)
   }
 
+  const handleChangeSearch = (event) => {
+    setSearchValue(event.target.value)
+  }
+
   return (
-    <AppBarStyled
-      position="static"
-      sx={{
-        '.MuiContainer-root': {
-          padding: '0 max(0.1em, calc((100% - clamp(300px, 95%, 1200px)) / 2))',
-        },
-      }}
-    >
+    <AppBarStyled position="static">
       <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'none' } }}>
+        <ToolbarStyled disableGutters variant="dense">
+          <Link href="/" id="logo-link">
+            <Logo>DeXy</Logo>
+          </Link>
+          {isDesktop && (
+            <Stack direction="row" gap={1} alignItems="center">
+              <EtheriumIcon />
+              <Typography fontSize="1.25rem">Etherium</Typography>
+            </Stack>
+          )}
+          <NavMenuContainer sx={{ display: { xs: 'flex', sm: 'none' } }}>
+            <SearchBar value={searchValue} handleChange={handleChangeSearch} />
             <IconButton
+              id="nav-menu-button"
               size="large"
-              aria-label="account of current user"
+              aria-label="menu"
               aria-controls="menu-appbar"
               aria-haspopup="true"
               onClick={handleOpenNavMenu}
@@ -84,25 +166,61 @@ function ResponsiveAppBar() {
                 </MenuItem>
               ))}
             </Menu>
-          </Box>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', sm: 'flex' } }}>
-            <EtheriumIcon />
+          </NavMenuContainer>
+          <NavMenuContainer sx={{ display: { xs: 'none', sm: 'flex' } }}>
+            <div
+              style={{
+                width: 'max(25%, 15rem)',
+                marginRight: 'min(1rem, 0.5vw)',
+                flexShrink: 8,
+              }}
+            >
+              <SearchBar value={searchValue} handleChange={handleChangeSearch} />
+            </div>
             {pages.map(([page, location]) => (
-              <Button
+              <NavButton
                 key={page}
                 onClick={() => handleClickMenu(location)}
-                sx={{
-                  my: 2,
-                  color: 'white',
-                  display: 'block',
-                  textDecoration: router.pathname === location ? 'underline' : 'none',
-                }}
+                className={router.pathname === location ? 'nav-button-active' : 'nav-button'}
               >
                 {page}
-              </Button>
+              </NavButton>
             ))}
-          </Box>
-        </Toolbar>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              sx={{ marginLeft: '1rem' }}
+            >
+              <AccountBalanceWalletRoundedIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+            >
+              <MenuItem onClick={() => {}}>Connect wallet</MenuItem>
+              {accountMenu.map(([page, location]) => (
+                <MenuItem key={`account-menu-page`} onClick={() => handleClickMenu(location)}>
+                  {page}
+                </MenuItem>
+              ))}
+              <MenuItem onClick={() => {}}>Log out</MenuItem>
+            </Menu>
+          </NavMenuContainer>
+        </ToolbarStyled>
       </Container>
     </AppBarStyled>
   )
