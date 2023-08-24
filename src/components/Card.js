2c536c22264aa676881305caa52143b34130d061
Quoc Mai <49741920+Legacy107@users.noreply.github.com>
2023-08-24 11:20:59 +1000

    Revert "Revert "Feature: [IN-42] add item details page""

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,41 +3,23 @@ import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
-import { CardActionArea, Stack, styled } from '@mui/material'
+import { CardActionArea, Stack } from '@mui/material'
 import EthereumIcon from '@/components/EthereumIcon'
 
-const TypographyStyledTitle = styled(Typography)(({ theme }) => ({
-  display: 'flex',
-  width: '135px',
-  height: '37px',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  flexShrink: 0,
-}))
-
-const TypographyStyledPrice = styled(Typography)(({ theme }) => ({
-  display: 'flex',
-  width: '46px',
-  height: '32px',
-  justifyContent: 'center',
-  alignItems: 'center',
-  flexShrink: 0,
-}))
-
-export default function ActionAreaCard({ image, title, price }) {
+export default function ActionAreaCard({ image, title, price, onClick }) {
   return (
     <Card sx={{ maxWidth: 180, height: 280 }}>
-      <CardActionArea>
+      <CardActionArea onClick={onClick}>
         <CardMedia component="img" height="180" image={image} alt={title} />
         <CardContent>
-          <TypographyStyledTitle gutterBottom variant="h5" component="div">
+          <Typography gutterBottom variant="h5" component="div">
             {title}
-          </TypographyStyledTitle>
-          <Stack direction="row">
-            <EthereumIcon size={13} />
-            <TypographyStyledPrice variant="body2" color="text.secondary">
+          </Typography>
+          <Stack direction="row" alignItems="center" gap={0.8}>
+            <EthereumIcon size={10} />
+            <Typography variant="body2" color="text.secondary">
               {price}
-            </TypographyStyledPrice>
+            </Typography>
           </Stack>
         </CardContent>
       </CardActionArea>
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,38 +1,34 @@
-import * as React from 'react'
-import Pagination from '@mui/material/Pagination'
-import Stack from '@mui/material/Stack'
-import { styled } from '@mui/material/styles'
+import { useState } from 'react'
+import { Pagination, Stack, styled } from '@mui/material'
 
-export default function PaginationButtons({ spacing = 10, pageCount = 4 }) {
-  const [page, setPage] = React.useState(1)
-
-  const StyledPagination = styled(Pagination)(({ theme }) => ({
+const StyledPagination = styled(Pagination)(({ theme }) => ({
+  '& .MuiPaginationItem-root': {
+    '&.Mui-selected': {
+      backgroundColor: theme.palette.primary.light,
+    },
+    '&:hover': {
+      backgroundColor: theme.palette.primary.light,
+    },
+  },
+  [theme.breakpoints.down('sm')]: {
     '& .MuiPaginationItem-root': {
-      '&.Mui-selected': {
-        backgroundColor: theme.palette.primary.dark,
-      },
-      '&:hover': {
-        // Add this block for hover styles
-        backgroundColor: theme.palette.primary.main, // Change this to the desired hover color
-      },
+      minWidth: '20px',
+      height: '20px',
+      lineHeight: '20px',
+      margin: '0 2px',
     },
-    [theme.breakpoints.down('sm')]: {
-      '& .MuiPaginationItem-root': {
-        fontSize: '0.7rem',
-        minWidth: '20px',
-        height: '20px',
-        lineHeight: '20px',
-        margin: '0 2px',
-      },
-    },
-  }))
+  },
+}))
 
-  const handlePageChange = (event, newPage) => {
+export default function PaginationButtons({ spacing = 10, pageCount = 4, ...props }) {
+  const [page, setPage] = useState(1)
+
+  const handlePageChange = (_event, newPage) => {
     setPage(newPage)
   }
 
   return (
-    <Stack spacing={spacing}>
+    <Stack spacing={spacing} {...props}>
       <StyledPagination
         page={page}
         count={pageCount}
