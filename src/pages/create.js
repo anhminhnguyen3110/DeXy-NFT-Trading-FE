2ed1ed7902987ba6b15ac31310be52afeddf96fe
Legacy107 <quocmai1107@gmail.com>
2023-09-22 17:58:08 +1000

    feat: [IN-83] connect create item api

--- src/pages/create.js
+++ src/pages/create.js
@@ -1,13 +1,15 @@
 /**
  * Author: Kien Quoc Mai, Anh Minh Nguyen, Duy Khoa Pham
  * Created date: 25/08/2023
- * Last modified Date: 29/08/2023
+ * Last modified Date: 22/09/2023
  */
+import { useState } from 'react'
 import Head from 'next/head'
-import { Button, Container, Stack, Typography, styled } from '@mui/material'
+import axios from '@/utils/axios'
+import { Container, Stack, Typography, Checkbox, styled } from '@mui/material'
+import { LoadingButton } from '@mui/lab'
 import InputFileUpload from '@/components/UploadButton'
 import InputBase from '@/components/InputBase'
-import CheckboxComponent from '@/components/CheckBox'
 import { useSnackbar } from 'notistack'
 
 const RootStyled = styled(Container)(() => ({
@@ -26,7 +28,7 @@ const UploadBox = styled(Stack)(({ theme }) => ({
   },
 }))
 
-const CreateButton = styled(Button)(({ theme }) => ({
+const CreateButton = styled(LoadingButton)(({ theme }) => ({
   alignSelf: 'flex-start',
   paddingInline: '2.5rem',
   [theme.breakpoints.down('sm')]: {
@@ -44,6 +46,59 @@ const InputBaseStyled = styled(InputBase)(({ theme }) => ({
  */
 export default function CreateDexyItem() {
   const { enqueueSnackbar } = useSnackbar()
+  const [loading, setLoading] = useState(false)
+  const [acceptRight, setAcceptRight] = useState(false)
+  const [acceptTerms, setAcceptTerms] = useState(false)
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [startPrice, setStartPrice] = useState(0)
+  const [fixPrice, setFixPrice] = useState(0)
+
+  const handleChangeRight = (event) => {
+    setAcceptRight(event.target.checked)
+  }
+
+  const handleChangeTerms = (event) => {
+    setAcceptTerms(event.target.checked)
+  }
+
+  const handleChangeName = (event) => {
+    setName(event.target.value)
+  }
+
+  const handleChangeDescription = (event) => {
+    setDescription(event.target.value)
+  }
+
+  const handleChangeStartPrice = (event) => {
+    setStartPrice(event.target.value)
+  }
+
+  const handleChangeFixPrice = (event) => {
+    setFixPrice(event.target.value)
+  }
+
+  const handleCreate = async () => {
+    if (!acceptRight || !acceptTerms) {
+      enqueueSnackbar('You must accept the terms and conditions', { variant: 'error' })
+      return
+    }
+
+    setLoading(true)
+    try {
+      const response = await axios.post('/items/create-item', {
+        name,
+        description,
+        start_price: startPrice,
+        fix_price: fixPrice,
+      })
+      enqueueSnackbar('Item created successfully', { variant: 'success' })
+    } catch (error) {
+      enqueueSnackbar('Failed to create item', { variant: 'error' })
+    } finally {
+      setLoading(false)
+    }
+  }
 
   return (
     <>
@@ -73,7 +128,11 @@ export default function CreateDexyItem() {
             Name
           </Typography>
           <Typography variant="body1">Choose a Dexy item unique name</Typography>
-          <InputBaseStyled placeholder="Enter the item’s name" />
+          <InputBaseStyled
+            placeholder="Enter the item’s name"
+            value={name}
+            onChange={handleChangeName}
+          />
         </Stack>
 
         <Stack gap={0.8}>
@@ -81,7 +140,12 @@ export default function CreateDexyItem() {
             Description
           </Typography>
           <Typography variant="body1">Describe your Dexy item for better understanding</Typography>
-          <InputBaseStyled placeholder="Enter the item’s description" multiline />
+          <InputBaseStyled
+            placeholder="Enter the item’s description"
+            multiline
+            value={description}
+            onChange={handleChangeDescription}
+          />
         </Stack>
 
         <Stack gap={0.8}>
@@ -89,7 +153,12 @@ export default function CreateDexyItem() {
             Start Price
           </Typography>
           <Typography variant="body1">Fill in your initial cost of Dexy item</Typography>
-          <InputBaseStyled placeholder="Enter the item’s start price" type="number" />
+          <InputBaseStyled
+            placeholder="Enter the item’s start price"
+            type="number"
+            value={startPrice}
+            onChange={handleChangeStartPrice}
+          />
         </Stack>
 
         <Stack gap={0.8}>
@@ -97,23 +166,30 @@ export default function CreateDexyItem() {
             Fix Price
           </Typography>
           <Typography variant="body1">Fill in your fixed cost to acquire Dexy item</Typography>
-          <InputBaseStyled placeholder="Enter the item’s fix price" type="number" />
+          <InputBaseStyled
+            placeholder="Enter the item’s fix price"
+            type="number"
+            value={fixPrice}
+            onChange={handleChangeFixPrice}
+          />
         </Stack>
 
         <div>
           <Stack direction="row" gap={1} alignItems="center">
-            <CheckboxComponent />
+            <Checkbox disableRipple checked={acceptRight} onChange={handleChangeRight} />
             <Typography variant="body1">
               I approve that I own and have rights of publication and sale of DeXy items
             </Typography>
           </Stack>
           <Stack direction="row" gap={1} alignItems="center">
-            <CheckboxComponent />
+            <Checkbox disableRipple checked={acceptTerms} onChange={handleChangeTerms} />
             <Typography variant="body1">I agree to the terms and conditions</Typography>
           </Stack>
         </div>
 
-        <CreateButton variant="contained">Create</CreateButton>
+        <CreateButton variant="contained" onClick={handleCreate} loading={loading}>
+          Create
+        </CreateButton>
       </RootStyled>
     </>
   )
