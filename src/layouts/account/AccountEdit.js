2fbe304a8a3aed90263873d016c547a5cddb1054
Legacy107 <quocmai1107@gmail.com>
2023-09-29 21:46:06 +1000

    feat: [IN-81] connect user api

--- src/layouts/account/AccountEdit.js
+++ src/layouts/account/AccountEdit.js
@@ -1,15 +1,15 @@
 /**
  * Author: Kien Quoc Mai
  * Created date: 19/08/2023
- * Last modified Date: 29/08/2023
+ * Last modified Date: 29/09/2023
  */
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import useResponsive from '@/hooks/useResponsive'
-import { Dialog, Avatar, IconButton, Button, styled } from '@mui/material'
+import { Dialog, Avatar, IconButton, styled } from '@mui/material'
+import { LoadingButton } from '@mui/lab'
 import EditRoundedIcon from '@mui/icons-material/EditRounded'
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded'
 import TextFieldWithLabel from '../../components/TextFieldWithLabel'
-import { useSnackbar } from 'notistack'
 
 const DialogStyled = styled(Dialog)(({ theme }) => ({
   '& .MuiDialog-paper': {
@@ -50,24 +50,57 @@ const CloseButton = styled(IconButton)(({ theme }) => ({
   color: theme.palette.grey[300],
 }))
 
+const UploadInput = styled('input')(({ theme }) => ({
+  position: 'absolute',
+  width: '100%',
+  height: '100%',
+  inset: '0',
+  opacity: '0',
+  cursor: 'pointer',
+}))
+
 /**
  *
+ * @param {object} userInfo user details
  * @param {boolean} open open state
  * @param {function} handleClose function to handle close
  * @param {function} handleSubmit function to handle submit account edit
  * @returns {JSX.Element}
  */
-export default function AccountEdit({ open, handleClose, handleSubmit }) {
-  const [avatar, setAvatar] = useState('')
-  const [username, setUsername] = useState('')
-  const [email, setEmail] = useState('')
+export default function AccountEdit({ userInfo, open, handleClose, handleSubmit }) {
+  const [avatar, setAvatar] = useState(userInfo.user_image)
+  const [username, setUsername] = useState(userInfo.user_name)
+  const [email, setEmail] = useState(userInfo.user_email)
   const fullScreen = useResponsive('down', 'sm')
-  const { enqueueSnackbar } = useSnackbar()
+  const [loading, setLoading] = useState(false)
+  const [avatarUrl, setAvatarUrl] = useState('')
 
-  const handleClickSubmit = () => {
-    handleSubmit(avatar, username, email)
-    handleClose()
-    enqueueSnackbar('Profile updated', { variant: 'success' })
+  useEffect(() => {
+    setAvatarUrl('')
+    setAvatar(userInfo.user_image)
+    setUsername(userInfo.user_name)
+    setEmail(userInfo.user_email)
+  }, [userInfo])
+
+  const handleClickSubmit = async () => {
+    setLoading(true)
+    try {
+      await handleSubmit(avatar, username, email)
+      handleClose()
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const handleAvatarChange = (event) => {
+    const file = event.target.files[0]
+    if (!file) return
+    setAvatar(file)
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      setAvatarUrl(e.target.result)
+    }
+    reader.readAsDataURL(file)
   }
 
   return (
@@ -76,10 +109,15 @@ export default function AccountEdit({ open, handleClose, handleSubmit }) {
         <CloseRoundedIcon />
       </CloseButton>
       <AvatarContainer>
-        <Avatar id="avatar-edit" sx={{ width: '120px', height: '120px' }} />
+        <Avatar
+          id="avatar-edit"
+          src={avatarUrl || avatar}
+          sx={{ width: '120px', height: '120px', pointerEvents: 'none' }}
+        />
         <IconButton id="avatar-edit-button">
           <EditRoundedIcon />
         </IconButton>
+        <UploadInput type="file" onChange={handleAvatarChange} />
       </AvatarContainer>
       <TextFieldWithLabel
         label="Username"
@@ -87,9 +125,9 @@ export default function AccountEdit({ open, handleClose, handleSubmit }) {
         onChange={(e) => setUsername(e.target.value)}
       />
       <TextFieldWithLabel label="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <Button variant="contained" onClick={handleClickSubmit}>
+      <LoadingButton variant="contained" onClick={handleClickSubmit} loading={loading}>
         Save
-      </Button>
+      </LoadingButton>
     </DialogStyled>
   )
 }
